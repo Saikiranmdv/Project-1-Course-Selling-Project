ac56f9fecc0f9b7f3187a47837041e0b4a2450bb
saikiran-modalavalasa <modalavalasasaikiran@gmail.com>
2024-11-17 16:15:34 +0530

    signin and signup endpoints

--- routes/admin.js
+++ routes/admin.js
@@ -1,25 +1,62 @@
 const {Router} = require("express")
 const adminRouter = Router()
 const {adminModel} = require("../db")
+const jwt = require("jsonwebtoken");
 
-function adminLogin(req, res, next){
-const username = req.body.username
-const password = req.body.password
+const JWT_ADMIN_PASSWORD = "12e434232edr324"
 
-next()
-}
+adminRouter.post("/signup", async function(req, res){
+    const { email, password, firstName, lastName } = req.body;
+  //Add Zod Validations
+  //hash the password as plain text is not stored in db
 
-adminRouter.post("/signup", function(req, res){
+  try {
+    await adminModel.create({
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
+    });
+
     res.json({
         message: "admin sign up"
     })
+  } catch (error) {
+    res.json({
+      message: `${error}`,
+    });
+  }
 })
 
 
-adminRouter.post("/signin", function(req, res){
+adminRouter.post("/signin", async function(req, res){
+    const { email, password } = req.body;
+
+  //ideally password should be hashed and hence cant compare user nad db password
+  const admin = await adminModel.findOne({
+    // finds all users with the username and password
+    email: email,
+    password: password,
+  });
+
+  if (admin) {
+    const token = jwt.sign(
+      {
+        id: admin._id,
+      },
+      JWT_ADMIN_PASSWORD
+    );
+
+    //if using cookies do cookie logic
     res.json({
-        message: "admin sign in"
-    })
+      token: token,
+    });
+  } else {
+    res.status(403).json({
+      message: "incorrect credentials",
+    });
+  }
+
 })
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,64 +1,76 @@
 const { Router } = require("express");
 const { userModel } = require("../db");
-const zod = require("zod")
+const zod = require("zod");
 
-const emailSchema = zod.string().email()
-const passwordSchema = zod.string().min(6)
+const emailSchema = zod.string().email();
+const passwordSchema = zod.string().min(6);
 //TODO : adding zod validation
 //TODO: hash the password so plaintxt is not stored
 
-const jwt = require("jsonwebtoken")
-const JWT_USER_PASSWORD = "keavbliebivh"
+const jwt = require("jsonwebtoken");
+const JWT_USER_PASSWORD = "keavbliebivh";
 
 const userRouter = Router();
 
-userRouter.post("/signup", async function(req, res){
-    const { email, password, firstName, lastName} = req.body
-    //Add Zod Validations
-    //hash the password as plain text is not stored in db
-    
-    try {
-        
+userRouter.post("/signup", async function (req, res) {
+  const { email, password, firstName, lastName } = req.body;
+  //Add Zod Validations
+  //hash the password as plain text is not stored in db
+
+  try {
     await userModel.create({
-        email: email,
-        password: password,
-        firstName: firstName,
-        lastName: lastName
-    })
-    
-    res.json({
-        message: "user signup endpoint succeded"
-    })
-    } catch (error) {
-        
-    }
-})
-
-userRouter.post("/signin", async function(req, res){
-    const { email, password } = req.body
-
-    const user = await userModel.find({
-        email: email,
-        password: password
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName,
     });
 
-    if (user) {
-        const token = jwt.sign({
-            id: user._id
-        }, JWT_USER_PASSWORD)
-    }
-
     res.json({
-        message: "user signin endpoint"
-    })
-})
-
-userRouter.get("/purchases", function(res, req, next){
+      message: "user signup endpoint succeded",
+    });
+  } catch (error) {
     res.json({
-        message: "user purchases endpoint" 
-    })
-})
+      message: `${error}`,
+    });
+  }
+});
+
+userRouter.post("/signin", async function (req, res) {
+  const { email, password } = req.body;
+
+  //ideally password should be hashed and hence cant compare user nad db password
+  const user = await userModel.findOne({
+    // finds all users with the username and password
+    email: email,
+    password: password,
+  });
+
+  if (user) {
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      JWT_USER_PASSWORD
+    );
+    
+    //if using cookies do cookie logic
+    res.json({
+      token: token,
+    });
+  } else {
+    res.status(403).json({
+      message: "incorrect credentials",
+    });
+  }
+
+});
+
+userRouter.get("/purchases", function (res, req, next) {
+  res.json({
+    message: "user purchases endpoint",
+  });
+});
 
 module.exports = {
-    userRouter: userRouter
-}
+  userRouter: userRouter,
+};
